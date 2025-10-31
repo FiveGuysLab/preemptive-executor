4d0bf405dbfd9c5ab319cacf7b7e3493e5f6009d
mehtaViraj <mehta.viraj.03@gmail.com>
2025-10-31 18:10:08 -0400

    add skeleton and other fixes

--- src/preemptive_executor.cpp
+++ src/preemptive_executor.cpp
@@ -1,5 +1,3 @@
-#include "../include/preemptive_executor/preemptive_executor.hpp"
-
 #include <chrono>
 #include <sched.h>
 #include <sys/syscall.h>
@@ -8,45 +6,25 @@
 #include <cstring>
 #include <iostream>
 #include <stdexcept>
+#include "preemptive_executor.hpp"
+#include <linux/sched.h>
 
 
 namespace preemptive_executor
 {
-    void set_sched_fifo_syscall(int priority, pid_t pid){
-        sched_attr attr{};
-        bool reset_on_fork = true; 
-        attr.size = sizeof(attr);
-        attr.sched_policy = SCHED_FIFO;
-        attr.sched_flags = reset_on_fork ? SCHED_FLAG_RESET_ON_FORK : 0;
-
-        if (sched_get_priority_min(SCHED_FIFO) > priority || sched_get_priority_max(SCHED_FIFO) < priority){
-            std::string priority_error = "priority " + std::to_string(priority) + " is in invalid range. Must be from 1-99."
-            throw std::runtime_error(priority_error);
-        }
-        attr.sched_priority= priority;
-        
-        long ret syscall(SYS_sched_setattr, pid, &attr, 0);
-
-        if (ret != 0){
-            throw std::system_error(errno, std::system_category(), "sched_setattr syscall failed");
-        }
+    void set_fifo_prio(int priority, std::thread& t){
+        const auto param = sched_param{priority};
+        pthread_setschedparam(t.native_handle(), SCHED_FIFO, &param); // TODO: We need to test this behaviour
     }
 
-    void worker_main(ThreadGroupAttributes* thread_group, WorkerGroup* worker_group){
-        //1: set timing policy
-        try {
-            //set SCHED FIFO prio using syscall
-            set_sched_fifo_syscall(thread_group->priority);
-        }
-        catch (const std::exception& e) {
-            std::cerr << "Failed to set priority with SCHED FIFO: " << e.what()
-                  << " (continuing with normal scheduling)\n";
-            throw;
-        }
-        //2: register with thread group
+    void worker_main(WorkerGroup& worker_group){
+
+        //1: set timing policy // NOTE: Handled by dispatcher
+        //2: register with thread group // NOTE: Registration handled by dispatcher
+
         while (true) {
             //3: wait on worker group semaphore
-            worker_group->semaphore->acquire();
+            worker_group.semaphore->acquire();
 
             if (!rclcpp::ok){
                 break;
@@ -56,73 +34,119 @@ namespace preemptive_executor
             rclcpp::AnyExecutable next_executable;
             bool has_executable = false;
             {
-               worker_group->ready_queue.mutex.lock();
-                if (!worker_group->ready_queue.queue.empty()) {
-                    next_executable = worker_group->ready_queue.queue.front(); // ref or ptr
-                    worker_group->ready_queue.queue.pop();
+               worker_group.ready_queue.mutex.lock();
+                if (!worker_group.ready_queue.queue.empty()) {
+                    next_executable = worker_group.ready_queue.queue.front(); // ref or ptr
+                    worker_group.ready_queue.queue.pop();
                     has_executable = true; // dont neeed
                 }
             }
 
             //6: unlock ready queue mutex 
-            worker_group->ready_queue.mutex.unlock();
+            worker_group.ready_queue.mutex.unlock();
 
             //7: execute executable (placeholder; actual execution integrates with executor run loop)
             if (has_executable) {
-                // what to do here?
+                // TODO: This is a non-static class member. I think our executor should pass the workers a lamba to call this
                 PreemptiveExecutor::execute_any_executable(next_executable);
             }
         }
     }
 
-    void* PreemptiveExecutor::get_callback_handle(const rclcpp::AnyExecutable& executable) {
-        //check which callback type is active 
-        if (executable.subscription) {
-            return executable.subscription->get_subscription_handle();
-        } else if (executable.timer) {
-            return executable.timer->get_timer_handle();
-        } else if (executable.service) {
-            return executable.service->get_service_handle();
-        } else if (executable.client) {
-            return executable.client->get_client_handle();
-        } else if (executable.waitable) {
-            return executable.waitable->get_handle();
-        } else {
-            return nullptr;
-        }
-    }
+    // NOTE: Can't use this. No way to make these types work. Needs more thought.
+    // void* PreemptiveExecutor::get_callback_handle(const rclcpp::AnyExecutable& executable) {
+    //     //check which callback type is active 
+    //     if (executable.subscription) {
+    //         return executable.subscription->get_subscription_handle();
+    //     } else if (executable.timer) {
+    //         return executable.timer->get_timer_handle();
+    //     } else if (executable.service) {
+    //         return executable.service->get_service_handle();
+    //     } else if (executable.client) {
+    //         return executable.client->get_client_handle();
+    //     } else if (executable.waitable) {
+    //         return executable.waitable->get_handle();
+    //     } else {
+    //         return nullptr;
+    //     }
+    // }
 
     void PreemptiveExecutor::spawn_worker_groups(){
-        //thread groups have number of threads as an int 
+        // thread groups have number of threads as an int 
         // iterate through vector of thread groups and spawn threads and populate one worker group per thread group
         for(auto& thread_group : thread_groups){
-            WorkerGroup* worker_group = new WorkerGroup();
-            std::vector<pid_t> thread_ids;
+            thread_group_id_worker_map.emplace(thread_group.tg_id, std::move(WorkerGroup()));
+            auto& worker_group = thread_group_id_worker_map.at(thread_group.tg_id);
 
             for (int i = 0; i < thread_group.number_of_threads; i++){
                 //spawn number_of_threads amount of threads and populate one worker group per thread
-                std::thread([this, thread_group, worker_group]() -> void {worker_main(thread_group, worker_group);}).detach();
-                //get thread id
-                std::thread::id thread_id = std::this_thread::get_id();
-                worker_group->thread_ids.push_back(thread_id);
+                auto t = new std::thread([&worker_group]() -> void {worker_main(worker_group);}); // pass in lamba to exec any executable. Or we could pass in this, but its a little excessive
+                worker_group.threads.push_back(t);
+                set_fifo_prio(thread_group.priority, *t);
+                t->detach();
             }
-            thread_group_id_worker_map[thread_group.tg_id] = worker_group;
         }
+    }
+
+    void PreemptiveExecutor::wait_for_work(std::chrono::nanoseconds timeout)
+    {
+    TRACETOOLS_TRACEPOINT(rclcpp_executor_wait_for_work, timeout.count());
+
+    // Clear any previous wait result
+    this->wait_result_.reset();
+
+    // we need to make sure that callback groups don't get out of scope
+    // during the wait. As in jazzy, they are not covered by the DynamicStorage,
+    // we explicitly hold them here as a bugfix
+    std::vector<rclcpp::CallbackGroup::SharedPtr> cbgs;
+
+    {
+        std::lock_guard<std::mutex> guard(mutex_);
+
+        if (this->entities_need_rebuild_.exchange(false) || current_collection_.empty()) {
+        this->collect_entities();
+        }
+
+        auto callback_groups = this->collector_.get_all_callback_groups();
+        cbgs.resize(callback_groups.size());
+        for(const auto & w_ptr : callback_groups) {
+        auto shr_ptr = w_ptr.lock();
+        if(shr_ptr) {
+            cbgs.push_back(std::move(shr_ptr));
+        }
+        }
+    }
+
+    this->wait_result_.emplace(wait_set_.wait(timeout));
+
+    // drop references to the callback groups, before trying to execute anything
+    cbgs.clear();
+
+    if (!this->wait_result_ || this->wait_result_->kind() == rclcpp::WaitResultKind::Empty) {
+        RCUTILS_LOG_WARN_NAMED(
+        "rclcpp",
+        "empty wait set received in wait(). This should never happen.");
+    } else {
+        if (this->wait_result_->kind() == rclcpp::WaitResultKind::Ready && current_notify_waitable_) {
+        auto & rcl_wait_set = this->wait_result_->get_wait_set().get_rcl_wait_set();
+        if (current_notify_waitable_->is_ready(rcl_wait_set)) {
+            current_notify_waitable_->execute(current_notify_waitable_->take_data());
+        }
+        }
+    }
     }
 
     void PreemptiveExecutor::spin() {
         spawn_worker_groups();
 
         //runs a while loop that calls wait for work
-        while(rclcpp::ok(this->context_)){
+        while(rclcpp::ok(context_) && spinning.load()) {
+            // TODO: Add a check for entity recollection. We don't support it, so throw if that state changes.
+
             rclcpp::AnyExecutable any_executable;
             wait_for_work(std::chrono::nanoseconds(-1));
-            
-            // Get callback handle using helper method
-            void* callback_handle = get_callback_handle(any_executable);
-            if (callback_handle && callback_id_worker_group_map.find(callback_handle) != callback_id_worker_group_map.end()) {
-                callback_id_worker_group_map[callback_handle]->semaphore->release();
-            }
+
+            // TODO: Post-processing the waitresult
         }
     }
 }
